a7af28805b0008aedc63f37f0554f5094de2acd2
leviable <levi.noecker@gmail.com>
2019-09-02 15:53:08 -0500

    Problem 0009 working

--- 0009-special-pythagorean-triplet/0009.go
+++ 0009-special-pythagorean-triplet/0009.go
@@ -1,8 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+type Triplet struct {
+	a, b, c int
+}
+
+func GetTriplets(target int) []Triplet {
+	foundTriplets := []Triplet{}
+	for z := target; z >= 3; z-- {
+		for y := z - 1; y >= 2; y-- {
+			for x := y - 1; x >= 1; x-- {
+				if IsTriplet(x, y, z) && x+y+z == target {
+					foundTriplets = append(foundTriplets, Triplet{x, y, z})
+				}
+			}
+		}
+	}
+
+	return foundTriplets
+}
+
 func IsTriplet(a, b, c int) bool {
 	if a > b || b > c {
 		return false
 	}
 	return a*a+b*b == c*c
+}
+
+func timeIt(start time.Time) {
+	elapsed := time.Since(start)
+	fmt.Printf("Completed in %s\n", elapsed)
+}
+
+func main() {
+	defer timeIt(time.Now())
+	triplets := GetTriplets(1000)
+	fmt.Printf("Found %v", triplets)
 }
--- 0009-special-pythagorean-triplet/0009_test.go
+++ 0009-special-pythagorean-triplet/0009_test.go
@@ -1,38 +1,38 @@
 package main
 
+import "fmt"
+import "reflect"
 import "testing"
 
 func TestIsTriplet(t *testing.T) {
-	t.Run("Is triplet 3-4-5", func(t *testing.T) {
-		got := IsTriplet(3, 4, 5)
-		want := true
+	potentialTriplets := []struct {
+		a, b, c int
+		expect  bool
+	}{
+		{a: 3, b: 4, c: 5, expect: true},
+		{a: 5, b: 12, c: 13, expect: true},
+		{a: 4, b: 3, c: 5, expect: false},
+		{a: 3, b: 5, c: 4, expect: false},
+	}
 
-		if got != want {
-			t.Errorf("Got %t, wanted %t", got, want)
-		}
-	})
-	t.Run("Is triplet 5-12-13", func(t *testing.T) {
-		got := IsTriplet(5, 12, 13)
-		want := true
+	for _, tt := range potentialTriplets {
+		t.Run(fmt.Sprintf("Evaluating %d, %d, %d", tt.a, tt.b, tt.c), func(t *testing.T) {
+			got := IsTriplet(tt.a, tt.b, tt.c)
+			want := tt.expect
+			if got != want {
+				t.Errorf("For %d-%d-%d got %t, want %t", tt.a, tt.b, tt.b, got, want)
+			}
+		})
+	}
+}
 
-		if got != want {
-			t.Errorf("Got %t, wanted %t", got, want)
-		}
-	})
-	t.Run("Is not triplet 4-3-5", func(t *testing.T) {
-		got := IsTriplet(4, 3, 5)
-		want := false
+func TestFindTriplets(t *testing.T) {
+	t.Run("Finding triplets equal to 12", func(t *testing.T) {
+		got := GetTriplets(12)
+		want := []Triplet{Triplet{3, 4, 5}}
 
-		if got != want {
-			t.Errorf("Got %t, wanted %t", got, want)
-		}
-	})
-	t.Run("Is not triplet 3-5-4", func(t *testing.T) {
-		got := IsTriplet(3, 5, 4)
-		want := false
-
-		if got != want {
-			t.Errorf("Got %t, wanted %t", got, want)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 }
