3cc304c4a9ba7a9d3447a547f8d8fefc076c2d80
leviable <levi.noecker@gmail.com>
2019-09-03 11:06:40 -0500

    All summing complete

--- 0011-largest-product-in-a-grid/0011.go
+++ 0011-largest-product-in-a-grid/0011.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
-
 func SumHorizontal(x, y, sumCount int, grid *[][]int) (sum int) {
 	for i := 0; i < sumCount; i++ {
-		val := (*grid)[x][y+i]
-		fmt.Printf("Val is %d\n", val)
+		val := (*grid)[y][x+i]
+		sum += val
+	}
+	return sum
+}
+
+func SumVertical(x, y, sumCount int, grid *[][]int) (sum int) {
+	for i := 0; i < sumCount; i++ {
+		val := (*grid)[y+i][x]
+		sum += val
+	}
+	return sum
+}
+
+func SumDiagonalRight(x, y, sumCount int, grid *[][]int) (sum int) {
+	for i := 0; i < sumCount; i++ {
+		val := (*grid)[y+i][x+i]
+		sum += val
+	}
+	return sum
+}
+
+func SumDiagonalLeft(x, y, sumCount int, grid *[][]int) (sum int) {
+	for i := 0; i < sumCount; i++ {
+		val := (*grid)[y+i][x-i]
 		sum += val
 	}
 	return sum
--- 0011-largest-product-in-a-grid/0011_test.go
+++ 0011-largest-product-in-a-grid/0011_test.go
@@ -9,13 +9,94 @@ var TestGrid = [][]int{
 	{20, 45, 35, 14},
 }
 
-func TestGridSumming(t *testing.T) {
-	t.Run("Sum horizontal", func(t *testing.T) {
-		got := SumHorizontal(0, 0, 2, &TestGrid)
-		want := 64
+func TestHorizontalGridSumming(t *testing.T) {
+	sumHoriz := []struct {
+		name                   string
+		x, y, sumCount, expect int
+	}{
+		{name: "Horizontal Sum 1", x: 3, y: 0, sumCount: 1, expect: 67},
+		{name: "Horizontal Sum 2", x: 2, y: 1, sumCount: 2, expect: 133},
+		{name: "Horizontal Sum 3", x: 1, y: 2, sumCount: 3, expect: 173},
+		{name: "Horizontal Sum 4", x: 0, y: 3, sumCount: 4, expect: 114},
+	}
 
-		if got != want {
-			t.Errorf("Got %v, wanted %v", got, want)
-		}
-	})
+	for _, tt := range sumHoriz {
+		t.Run(tt.name, func(t *testing.T) {
+			got := SumHorizontal(tt.x, tt.y, tt.sumCount, &TestGrid)
+			want := tt.expect
+
+			if got != want {
+				t.Errorf("Got %v, wanted %v", got, want)
+			}
+		})
+	}
+}
+
+func TestVerticalGridSumming(t *testing.T) {
+	sumHoriz := []struct {
+		name                   string
+		x, y, sumCount, expect int
+	}{
+		{name: "Vertical Sum 1", x: 0, y: 3, sumCount: 1, expect: 20},
+		{name: "Vertical Sum 2", x: 1, y: 2, sumCount: 2, expect: 62},
+		{name: "Vertical Sum 3", x: 2, y: 1, sumCount: 3, expect: 207},
+		{name: "Vertical Sum 4", x: 3, y: 0, sumCount: 4, expect: 198},
+	}
+
+	for _, tt := range sumHoriz {
+		t.Run(tt.name, func(t *testing.T) {
+			got := SumVertical(tt.x, tt.y, tt.sumCount, &TestGrid)
+			want := tt.expect
+
+			if got != want {
+				t.Errorf("Got %v, wanted %v", got, want)
+			}
+		})
+	}
+}
+
+func TestDiagonalRightGridSumming(t *testing.T) {
+	sumHoriz := []struct {
+		name                   string
+		x, y, sumCount, expect int
+	}{
+		{name: "Diagonal Right Sum 1", x: 3, y: 3, sumCount: 1, expect: 14},
+		{name: "Diagonal Right Sum 2", x: 2, y: 2, sumCount: 2, expect: 92},
+		{name: "Diagonal Right Sum 3", x: 1, y: 1, sumCount: 3, expect: 155},
+		{name: "Diagonal Right Sum 4", x: 0, y: 0, sumCount: 4, expect: 181},
+	}
+
+	for _, tt := range sumHoriz {
+		t.Run(tt.name, func(t *testing.T) {
+			got := SumDiagonalRight(tt.x, tt.y, tt.sumCount, &TestGrid)
+			want := tt.expect
+
+			if got != want {
+				t.Errorf("Got %v, wanted %v", got, want)
+			}
+		})
+	}
+}
+
+func TestDiagonalLeftGridSumming(t *testing.T) {
+	sumHoriz := []struct {
+		name                   string
+		x, y, sumCount, expect int
+	}{
+		{name: "Diagonal Left Sum 1", x: 0, y: 3, sumCount: 1, expect: 20},
+		{name: "Diagonal Left Sum 2", x: 1, y: 2, sumCount: 2, expect: 37},
+		{name: "Diagonal Left Sum 3", x: 2, y: 1, sumCount: 3, expect: 131},
+		{name: "Diagonal Left Sum 4", x: 3, y: 0, sumCount: 4, expect: 198},
+	}
+
+	for _, tt := range sumHoriz {
+		t.Run(tt.name, func(t *testing.T) {
+			got := SumDiagonalLeft(tt.x, tt.y, tt.sumCount, &TestGrid)
+			want := tt.expect
+
+			if got != want {
+				t.Errorf("Got %v, wanted %v", got, want)
+			}
+		})
+	}
 }
