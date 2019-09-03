778316dfbe26a5b3cafeb80638be6b2269732c6c
leviable <levi.noecker@gmail.com>
2019-09-03 14:14:03 -0500

    Validation and error checking complete

--- 0011-largest-product-in-a-grid/0011.go
+++ 0011-largest-product-in-a-grid/0011.go
@@ -1,33 +1,49 @@
 package main
 
-func SumHorizontal(x, y, sumCount int, grid *[][]int) (sum int) {
+import "errors"
+
+var GridIndexError = errors.New("Grid index out of bounds")
+
+func SumHorizontal(x, y, sumCount int, grid *[][]int) (sum int, err error) {
 	for i := 0; i < sumCount; i++ {
+		if x+i >= len(*grid) || x < 0 || y < 0 {
+			return 0, GridIndexError
+		}
 		val := (*grid)[y][x+i]
 		sum += val
 	}
-	return sum
+	return sum, nil
 }
 
-func SumVertical(x, y, sumCount int, grid *[][]int) (sum int) {
+func SumVertical(x, y, sumCount int, grid *[][]int) (sum int, err error) {
 	for i := 0; i < sumCount; i++ {
+		if y+i >= len(*grid) || y < 0 || x < 0 {
+			return 0, GridIndexError
+		}
 		val := (*grid)[y+i][x]
 		sum += val
 	}
-	return sum
+	return sum, nil
 }
 
-func SumDiagRight(x, y, sumCount int, grid *[][]int) (sum int) {
+func SumDiagRight(x, y, sumCount int, grid *[][]int) (sum int, err error) {
 	for i := 0; i < sumCount; i++ {
+		if x+i >= len(*grid) || y+i >= len(*grid) || y < 0 || x < 0 {
+			return 0, GridIndexError
+		}
 		val := (*grid)[y+i][x+i]
 		sum += val
 	}
-	return sum
+	return sum, nil
 }
 
-func SumDiagLeft(x, y, sumCount int, grid *[][]int) (sum int) {
+func SumDiagLeft(x, y, sumCount int, grid *[][]int) (sum int, err error) {
 	for i := 0; i < sumCount; i++ {
+		if x-i < 0 || y+i >= len(*grid) || y < 0 || x < 0 {
+			return 0, GridIndexError
+		}
 		val := (*grid)[y+i][x-i]
 		sum += val
 	}
-	return sum
+	return sum, nil
 }
--- 0011-largest-product-in-a-grid/0011_test.go
+++ 0011-largest-product-in-a-grid/0011_test.go
@@ -10,9 +10,9 @@ var TestGrid = [][]int{
 }
 
 func TestGridSumming(t *testing.T) {
-	sumHoriz := []struct {
+	sumVals := []struct {
 		name                   string
-		fn                     func(int, int, int, *[][]int) int
+		fn                     func(int, int, int, *[][]int) (int, error)
 		x, y, sumCount, expect int
 	}{
 		{name: "Horizontal Sum 1", fn: SumHorizontal, x: 3, y: 0, sumCount: 1, expect: 67},
@@ -33,13 +33,52 @@ func TestGridSumming(t *testing.T) {
 		{name: "Diagonal Left Sum 4", fn: SumDiagLeft, x: 3, y: 0, sumCount: 4, expect: 198},
 	}
 
-	for _, tt := range sumHoriz {
+	for _, tt := range sumVals {
 		t.Run(tt.name, func(t *testing.T) {
-			got := tt.fn(tt.x, tt.y, tt.sumCount, &TestGrid)
+			got, err := tt.fn(tt.x, tt.y, tt.sumCount, &TestGrid)
 			want := tt.expect
+
+			if err != nil {
+				t.Fatalf("Got an error %q and didn't expect one", err)
+			}
 
 			if got != want {
 				t.Errorf("Got %v, wanted %v", got, want)
+			}
+		})
+	}
+}
+
+func TestGridSearchOutOfBounds(t *testing.T) {
+	sumVals := []struct {
+		name           string
+		fn             func(int, int, int, *[][]int) (int, error)
+		x, y, sumCount int
+	}{
+		{name: "Horizontal OOB 1", fn: SumHorizontal, x: 3, y: 0, sumCount: 2},
+		{name: "Horizontal OOB 2", fn: SumHorizontal, x: -1, y: 0, sumCount: 2},
+		{name: "Horizontal OOB 3", fn: SumHorizontal, x: 1, y: -1, sumCount: 2},
+		{name: "Vertical OOB 1", fn: SumVertical, x: 3, y: 3, sumCount: 2},
+		{name: "Vertical OOB 2", fn: SumVertical, x: -1, y: 0, sumCount: 2},
+		{name: "Vertical OOB 3", fn: SumVertical, x: 1, y: -1, sumCount: 2},
+		{name: "DiagonalRight OOB 1", fn: SumDiagRight, x: 3, y: 3, sumCount: 2},
+		{name: "DiagonalRight OOB 2", fn: SumDiagRight, x: 2, y: 3, sumCount: 2},
+		{name: "DiagonalRight OOB 3", fn: SumDiagRight, x: 3, y: 2, sumCount: 2},
+		{name: "DiagonalRight OOB 4", fn: SumDiagRight, x: 0, y: -1, sumCount: 2},
+		{name: "DiagonalRight OOB 5", fn: SumDiagRight, x: 1, y: -1, sumCount: 2},
+		{name: "DiagonalLeft OOB 1", fn: SumDiagLeft, x: 0, y: 3, sumCount: 2},
+		{name: "DiagonalLeft OOB 2", fn: SumDiagLeft, x: 1, y: 3, sumCount: 2},
+		{name: "DiagonalLeft OOB 3", fn: SumDiagLeft, x: 0, y: 2, sumCount: 2},
+		{name: "DiagonalLeft OOB 4", fn: SumDiagLeft, x: 0, y: -1, sumCount: 2},
+		{name: "DiagonalLeft OOB 5", fn: SumDiagLeft, x: 1, y: -1, sumCount: 2},
+	}
+	for _, tt := range sumVals {
+		t.Run(tt.name, func(t *testing.T) {
+			_, got := tt.fn(tt.x, tt.y, tt.sumCount, &TestGrid)
+			want := GridIndexError
+
+			if got != want {
+				t.Errorf("Got error %q, wanted %q", got, want)
 			}
 		})
 	}
